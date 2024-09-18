83f4864f8b1623c20471ef45eeb8d9291d7ed534
Muhamad Amir <m.amirazmi@hotmail.com>
2024-09-18 17:54:58 +0800

    Remove playground

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { Form } from "./components/form";
-// import { Playground } from "./playground";
 
 export const App = () => {
   return (
     <div className="p-6 bg-zinc-100 min-h-screen">
-      {/* <Playground /> */}
       <Form />
     </div>
   );
